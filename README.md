5bac1c68c571bf62a6a8dd6170d7388ed8f4a1c3
jMyles <justin@justinholmes.com>
2021-02-07 08:52:58 -0500

    Some updates for 20.04 and beyond.

--- README.md
+++ README.md
@@ -2,9 +2,15 @@ Some good automated stuff.  Some stuff is still more easily done manually.
 
 Here's what we do:
 
-./first-without-sudo.sh
-sudo ./second-with-sudo.sh
-sudo ansible-playbook basic.yml -vvvv
-./set-favs.sh
+* sudo apt update
+* sudo apt dist-upgrade
+* sudo apt install git
+* git clone https://github.com/jMyles/jmyles-ubuntu-spinup
+* cd jmyles-ubuntu-spinup
+* ./first-without-sudo.sh
+* Check email for dashlane code
+* sudo ./second-with-sudo.sh
+* sudo ansible-playbook basic.yml -vvvv
+* ./set-favs.sh
 
 Copy PyCharm config
--- first-without-sudo.sh
+++ first-without-sudo.sh
@@ -1,10 +1,14 @@
 firefox "https://www.dashlane.com/download"
 cd
-ssh-keygen -f ~/.ssh/id_rsa; curl -u "jMyles" --data '{"title":"'"$HOSTNAME"'","key":"'"`cat ~/.ssh/id_rsa.pub`"'"}' https://api.github.com/user/keys --header 'x-github-otp:<get a two-factor auth code and put it here.>'
+ssh-keygen -f ~/.ssh/id_rsa
 mkdir projects
 cd projects
-git clone git@github.com:jMyles/jmyles-ubuntu-spinup.git
-cd jmyles-ubuntu-spinup
+
+# Make a few aliases
+
 echo "alias pretty=\"git log --pretty='%C(yellow)%h %<(10,trunc)%Cgreen%aN %Cred%d %Creset %<(80,trunc)%s'\"" >> ~/.bashrc
 echo "alias jholmes=\"ssh -o 'StrictHostKeyChecking no' 'jmyles_justinholmescom@ssh.phx.nearlyfreespeech.net'\"" >> ~/.bashrc
 echo "export jh.com=jmyles_justinholmescom@ssh.phx.nearlyfreespeech.net" >> ~/.bashrc
+
+# Ideally, we add the SSH key to github here, but it doesn't seem to auth?
+# curl -u "jMyles" --data '{"title":"'"$HOSTNAME"'","key":"'"`cat ~/.ssh/id_rsa.pub`"'"}' https://api.github.com/user/keys --header 'x-github-otp:<get a two-factor auth code and put it here.>'
